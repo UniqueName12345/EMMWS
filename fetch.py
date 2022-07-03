85202086debb4ecd32f9407e718d0a5e22c8e9fa
Chiroyce <97374054+Chiroyce1@users.noreply.github.com>
2022-07-03 19:01:11 +0530

    Create src folder

--- fetch.py
+++ fetch.py
@@ -2,5 +2,5 @@ from requests import get
 
 SOURCE = "https://scratch.mit.edu/discuss/post/6429679/source/"
 
-with open('./main.js', 'w') as file:
+with open('./src/main.js', 'w') as file:
     file.write(get(SOURCE).text)
