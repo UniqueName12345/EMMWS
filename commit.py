599f4c9b6d9bcc80d8a1d18fa0545bb5d742a4f0
Chiroyce <97374054+Chiroyce1@users.noreply.github.com>
2022-07-09 11:02:05 +0530

    Fix KeyError with commit message

--- commit.py
+++ commit.py
@@ -2,4 +2,4 @@ from requests import get
 from subprocess import run
 
 commit_info = get("https://scratch.mit.edu/discuss/post/6444284/source/").json()
-run(["git", "commit", "-am", f"{commit_info['commit_msg']} - {commit_info['version']}"])
+run(["git", "commit", "-am", f"{commit_info['commit_message']} - {commit_info['version']}"])
