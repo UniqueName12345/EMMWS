eee531fd238b86013b55658d98c61220fe7a3124
Chiroyce <97374054+Chiroyce1@users.noreply.github.com>
2022-07-10 14:24:07 +0530

    Try to keep the repl alive

--- fetch.py
+++ fetch.py
@@ -4,3 +4,7 @@ SOURCE = "https://scratch.mit.edu/discuss/post/6429679/source/"
 
 with open('./src/main.js', 'w') as file:
     file.write(get(SOURCE).text)
+
+get("https://mmws.chiroyce.repl.co/", headers={
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36."
+})
